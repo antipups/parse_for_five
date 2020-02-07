ca2fc17f03c794e8562ad0e4b78eabe7e2a8d44d
antipups <nikitakurkurin@protonmail.ch>
2020-02-07 18:16:43 +0300

    Сделан парс имени, и добавлена функция для облегчения обрезки.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
-import pprint
+
+
+# функция резалка, string - какую строку режем, afterstring - с какого мента режем
+cut = lambda string, afterstring: string[string.find(afterstring) + len(afterstring) + 2:]
+
+
 
 
 headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
@@ -7,6 +12,22 @@ headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 
 
 html = requests.get('https://www.ces.tech/api/Exhibitors?searchTerm=&sortBy=alpha&filter=&pageNo=1&pageSize=30',
-                    headers=headers)
-print(html)
+                    headers=headers).text
+html = html[html.find('"exhibitors":') + 13:]
+
+
+while True:
+
+    current_company = html[:html.find(',{"companyName"')]   # теперь current company - эта вся рассматриваемая компания
+    print(current_company)
+    name_company = current_company[current_company.find('"companyName":"') + 15:current_company.find('","')]
+    current_company = cut(current_company, name_company)
+    # print(current_company)
+    # description_company =
+    break
+
+    if html.find('{"companyName"') > -1:
+        html = html[html.find('{"companyName"') + 1:]
+    else:
+        break
 
